34823a545e415a78c7a684573449667f6c40b8bc
amruthalalam <144026974+amruthalalam@users.noreply.github.com>
2026-02-05 21:25:35 +0530

    Update pthread_barrier.c

--- pthread_barrier.c
+++ pthread_barrier.c
@@ -7,9 +7,7 @@ pthread_barrier_t barrier;
 void* task(void* arg) {
     int id = *(int*)arg;
     printf("Thread %d before barrier\n", id);
-    sleep(1); // simulate some work
-
-    // wait at the barrier
+    sleep(1);
     pthread_barrier_wait(&barrier);
 
     printf("Thread %d after barrier\n", id);
@@ -19,22 +17,15 @@ void* task(void* arg) {
 int main() {
     pthread_t threads[NUM_THREADS];
     int ids[NUM_THREADS];
-
-    // Initialize barrier for NUM_THREADS
     pthread_barrier_init(&barrier, NULL, NUM_THREADS);
 
-    // Create threads
     for (int i = 0; i < NUM_THREADS; i++) {
         ids[i] = i + 1;
         pthread_create(&threads[i], NULL, task, &ids[i]);
     }
-
-    // Join threads
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
-
-    // Destroy barrier
     pthread_barrier_destroy(&barrier);
 
     return 0;
